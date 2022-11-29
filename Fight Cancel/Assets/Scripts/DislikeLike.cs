60924872117b3b98fd605b3e9c9cc3edb8aebcce
sarais02 <sarais02@ucm.es>
2022-11-29 20:44:12 +0100

    Refactorizacion sin uAdventure

---
+++ Fight Cancel/Assets/Scripts/DislikeLike.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DislikeLike : MonoBehaviour
+{
+    public bool isDislike;
+    public int followers;
+    [SerializeField]
+    Image like, dislike;
+    [SerializeField]
+    Sprite likePressed, dislikePressed;
+    //void OnMouseDown()
+    //{
+    //    Debug.Log("lkjhgfd");
+    //    if (GameManager.GetInstance() != null)
+    //    {
+    //        if (dislike)
+    //            GameManager.GetInstance().LessFollowers(followers);
+    //        else
+    //            GameManager.GetInstance().AddFollowers(followers);
+    //    }
+    //}
+
+    public void DislikeLikeClick()
+    {
+        if (GameManager.GetInstance() != null)
+        {
+            if (isDislike)
+            {
+                GameManager.GetInstance().LessFollowers(followers);
+                dislike.sprite = dislikePressed;
+            }
+            else
+            {
+                GameManager.GetInstance().AddFollowers(followers);
+                like.sprite = likePressed;
+            }
+        }
+    }
+}
---
+++ Fight Cancel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    private UIManager UIManager;
+    //public static GameManager instance = null;
+
+    int seguidores = 200;
+    int post = 0;
+    int likes = 0, dislikes = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+            Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        //LessFollowers(1);
+    }
+
+    public void LessFollowers(int lostFollowers)
+    {
+        seguidores -= lostFollowers;
+
+        if (seguidores <= 0)
+            seguidores = 0;
+    }
+
+    public void AddFollowers(int newFollowers)
+    {
+        seguidores += newFollowers;
+    }
+
+    public bool HowManyPost()
+    {
+        return post < 1;
+    }
+
+    public void AddPost()
+    {
+        post++;
+    }
+
+    public void UIManagerUpdate(UIManager uim)
+    {
+        UIManager = uim;
+        UIManager.UpdateFollowers(seguidores);
+    }
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+}
