98ced0d7b8c1326d0b54fd99afae0997f565cf3b
EvSanz <evsanc07@ucm.es>
2022-12-07 16:41:40 +0100

    Publicacion de fotos rudimentaria

--- Fight Cancel/Assets/Scripts/Feed/Post.cs
+++ Fight Cancel/Assets/Scripts/Feed/Post.cs
@@ -6,12 +6,28 @@ using UnityEngine;
 public class Post : MonoBehaviour
 {
     public GameObject post, panelOpciones; 
-    public Text postTexto, comentarioTexto; 
+    public Text postTexto, comentarioTexto;
+    public Image postImage, comentarioImage; 
 
     public void PostText()
     {
-        comentarioTexto.text = postTexto.text;
+        if (postTexto != null)
+            comentarioTexto.text = postTexto.text;
+
+        DesactivarPaneles(); 
+    }
+
+    public void PostImage()
+    {
+        if (postImage != null)
+            comentarioImage.sprite = postImage.sprite;
+
+        DesactivarPaneles(); 
+    }
+
+    void DesactivarPaneles()
+    {
         post.SetActive(true);
-        panelOpciones.SetActive(false); 
+        panelOpciones.SetActive(false);
     }
 }
